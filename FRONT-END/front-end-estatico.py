07220e53324c3bd55ce5084fe5bf742609297971
Gabriel Gomes <gabrielgomes38@hotmail.com>
2022-01-24 19:32:53 -0300

    Arquivos Projetos

---
+++ FRONT-END/front-end-estatico.py
@@ -0,0 +1,27 @@
+"""
+O que vamos aprender?
+
+#Git != GitHub
+#HTML
+#CSS(Seletores)
+#CSS(Estilos)
+#CSS(flex/Grid)
+
+Git = É responsavel pelo 
+
+GitHub = Repositório de códigos
+    MASTER = é o que o úsuario esta visualizando
+    BRANCH =
+    MERGE = junta os BRANCH com a MASTER
+
+
+HTML =
+    não é linguagem de programação
+    Marcação de texto
+
+CSS =
+    Estilo da página
+
+JAVASCRIPT = 
+    Usado para poder dar funcionalidadepara a página
+"""
